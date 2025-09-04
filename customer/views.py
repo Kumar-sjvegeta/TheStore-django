e21cc294a0b99d414094f4a57542d0df48928e18
Kumar-sjvegeta <kumar.sjvegeta@gmail.com>
2025-09-05 01:35:30 +0530

    modified the product view to throw all the product data in json

--- customer/views.py
+++ customer/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 from staff.models import Inventory
+from django.core import serializers
+import json
 # Create your views here.
 
 def home(request):
@@ -8,4 +10,12 @@ def home(request):
 
 def products(request):
     all_products = Inventory.objects.all()
-    return HttpResponse(all_products)
+    print(f"all_products --> {all_products}")
+    all_products_serialized = serializers.serialize('json', all_products)
+    print(f"all_products_serialized --> {all_products_serialized}")
+    json_data = json.loads(all_products_serialized)
+    print(f"json_data ---> {json_data}")
+    pretty_json_data = json.dumps(json_data, indent=4)
+    print(f"pretty_json_data --> {pretty_json_data}")
+
+    return HttpResponse(pretty_json_data, content_type=json)
