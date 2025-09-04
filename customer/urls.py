c6ee9ca4e1eb5ce9a266d3e8b39e2e2117491766
Kumar-sjvegeta <kumar.sjvegeta@gmail.com>
2025-09-04 22:19:35 +0530

    created new homepage template for customer app

--- customer/urls.py
+++ customer/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path("home/", views.home, name="customer_home")
+    path("home/", views.home, name="customer_home"),
+    path('products/', views.products, name="customer_products")
 ]
--- customer/views.py
+++ customer/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseRedirect, Http404
-
+from staff.models import Inventory
 # Create your views here.
 
 def home(request):
-    return HttpResponse("This is the homepage of the customers app in TheStore project")
+    return render(request, 'home.html')
+
+def products(request):
+    all_products = Inventory.objects.all()
+    return HttpResponse(all_products)
