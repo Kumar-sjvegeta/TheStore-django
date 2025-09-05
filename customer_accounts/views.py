2f8a3d481fb30e768458e891b9aac537f878324f
Kumar-sjvegeta <kumar.sjvegeta@gmail.com>
2025-09-05 14:55:21 +0530

    form ready. able to get data to backend. need to implement login() method

--- customer_accounts/views.py
+++ customer_accounts/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth import authenticate
 
 # Create your views here.
+# superuser - kumar
+# password - 12345
 
 def customer_login_page(request):
     return render(request, 'customer_accounts/login_page.html')
@@ -14,6 +17,13 @@ def customer_login_action(request):
         entered_pass = request.POST.get('password')
         print(f"Entered username --> {entered_user}")
         print(f"Entered password --> {entered_pass}")
+        user = authenticate(request, username=entered_user, password=entered_pass)
+        print(f"type of user --> {type(user)}")
+        print(f"user --> {user}")
+        if user is None:
+            print("invalid credentials")
+        else:
+            print("Welcome ! ")
 
         return redirect('customer_login_page')
 
