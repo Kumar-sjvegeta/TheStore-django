cad4a24049f820ce2fbfc0db9066790c55751759
Kumar-sjvegeta <kumar.sjvegeta@gmail.com>
2025-09-05 14:26:10 +0530

    basic html form is ready.

--- customer_accounts/urls.py
+++ customer_accounts/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("customer_login/", views.customer_login_page, name='customer_login_page')
+    path("customer_login/", views.customer_login_page, name='customer_login_page'),
+    path("customer_login/login_action", views.customer_login_action, name='customer_login_action')
 ]
--- customer_accounts/views.py
+++ customer_accounts/views.py
@@ -5,3 +5,17 @@ from django.http import HttpResponse
 
 def customer_login_page(request):
     return render(request, 'customer_accounts/login_page.html')
+
+def customer_login_action(request):
+    print(f"request.method ---> {request.method}")
+    if request.method == 'POST':
+        print(f"request.POST ---> {request.POST}")
+        entered_user = request.POST.get('username')
+        entered_pass = request.POST.get('password')
+        print(f"Entered username --> {entered_user}")
+        print(f"Entered password --> {entered_pass}")
+
+        return redirect('customer_login_page')
+
+
+    return render(request, 'customer_accounts/login_page.html')
